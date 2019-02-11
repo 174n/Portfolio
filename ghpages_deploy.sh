19e645183ee9048e291b84d15399477bf5d91e0a
Ivan Alexandrov <ivan@alexandrov.online>
2019-02-11 17:50:40 +0100

    configure deployment

--- ghpages_deploy.sh
+++ ghpages_deploy.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-git checkout master
-git subtree split --prefix dist -b gh-pages
-git push -f gh-pages origin:gh-pages
-git branch -D gh-pages
+git subtree push --prefix dist origin gh-pages
